4865e617d2a71b34d48151a9d06726e9ac98d060
Grace Newman <grace.newman@digital.hmrc.gov.uk>
2017-12-04 16:09:55 +0000

    Edit to Hangman

--- hangman.py
+++ hangman.py
@@ -3,33 +3,61 @@ my_dict = ["discussion", "policeman", "mathematics", "exchange", "mysterious"]
 from random import randint
 
 word = ""
-def word_selector(my_dict):
+char_guesses = ""
+wrong_turn = 0
+
+def word_selector(my_dict, wrong_turn):
     word_num = randint(0, len(my_dict) - 1)
     print word_num
     word = my_dict[word_num]
     word = word.upper()
     print word
     print "_" * len(word)
-    user_guess()
+    user_guess(word, char_guesses, wrong_turn)
 
 
-char = ""
-def user_guess():
-    char = raw_input("Guess a letter: ")
-    char = char.upper()
-    print char
-    correct_guess(word, char)
+guess = ""
 
-def correct_guess(word, char):
+def user_guess(word, char_guesses, wrong_turn):
+        while wrong_turn < 10:
+            if wrong_turn == "":
+                break
+            else:
+                guess = raw_input("Guess a letter: ")
+                guess = guess.upper()
+
+                if guess == "":
+                    print "Guess invalid. Please enter a letter."
+                elif len(guess) > 1:
+                    print "Guess invalid. Please enter only one letter."
+                elif guess in char_guesses:
+                    print "You already guessed that! Try again."
+                else:
+                    char_guesses += guess
+                    wrong_turn = correct_guess(word, char_guesses, guess, wrong_turn)
+
+
+def correct_guess(word, char_guesses, guess, wrong_turn):
+    i_wrong_turn = wrong_turn
+    wrong = 0
     for letter in word:
-        if letter != char:
-            print "_",
+        if letter in char_guesses:
+            print letter,
         else:
-            print char,
-    print
+            print "_",
+            wrong += 1
+    if guess not in word:
+        i_wrong_turn += 1
+        print "Wrong"
+        print i_wrong_turn
+        if i_wrong_turn == 10:
+            print "Sorry! You ran out of turns!"
+    else:
+        print "You got it right"
+        if wrong == 0:
+            print "You won!"
+            return ""
+    return i_wrong_turn
 
 
-
-word_selector(my_dict)
-# user_guess()
-# correct_guess(word)
+word_selector(my_dict, wrong_turn)
